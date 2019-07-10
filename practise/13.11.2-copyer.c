6733eb23bf89d4716d13bb63eb9bed161303e6b8
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-10 10:52:16 +0800

    Update 13.11.2-copyer.c

--- practise/13.11.2-copyer.c
+++ practise/13.11.2-copyer.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define NAMELEN 81
-#define BUF 4096
 
 int main(int argc, char * argv[])
 {
     char file_dest[NAMELEN];
     char file_source[NAMELEN];
-    static char temp[BUF];
+    int ch;
     FILE * fs, * fd;
 
     if (argc != 3)          //命令行参数不为3时
@@ -16,14 +15,14 @@ int main(int argc, char * argv[])
         exit(EXIT_FAILURE);
     }
 
-    if ((fd = fopen(argv[1], "wb+")) == NULL && (fs = fopen(argv[2], "rb")) == NULL)
+    if ((fd = fopen(argv[1], "w+")) == NULL && (fs = fopen(argv[2], "r")) == NULL)
     {
         fprintf(stderr, "Wrong in opening dest/source file.\n");
         exit(EXIT_FAILURE);             //打开文件失败时
     }                                   //记得关文件
 
-    while (fgets(temp, BUF, fs) != NULL)
-        fputs(temp, fd);
+    while ((ch = getc(fs)) != EOF)
+        putc(ch, fd);
     
     fclose(fd);
     fclose(fs);
