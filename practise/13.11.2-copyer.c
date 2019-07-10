3a6859c7a7ee1ffc2605c97050150ae53a5c032f
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-10 10:38:29 +0800

    Create 13.11.2-copyer.c

---
+++ practise/13.11.2-copyer.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NAMELEN 81
+#define BUF 4096
+
+int main(int argc, char * argv[])
+{
+    char file_dest[NAMELEN];
+    char file_source[NAMELEN];
+    static char temp[BUF];
+    FILE * fs, * fd;
+
+    if (argc != 3)          //命令行参数不为3时
+    {
+        fprintf(stderr, "Usage: %s [dest_file] [source_file]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((fd = fopen(argv[1], "wb+")) == NULL && (fs = fopen(argv[2], "rb")) == NULL)
+    {
+        fprintf(stderr, "Wrong in opening dest/source file.\n");
+        exit(EXIT_FAILURE);             //打开文件失败时
+    }                                   //记得关文件
+
+    while (fgets(temp, BUF, fs) != NULL)
+        fputs(temp, fd);
+    
+    fclose(fd);
+    fclose(fs);
+
+    puts("Done");
+    
+    system("pause");
+    return 0;
+}
