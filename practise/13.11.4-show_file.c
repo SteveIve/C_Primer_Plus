98a1aafb72db71d4733593cd53cf688845022577
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-15 21:34:47 +0800

    Create 13.11.4-show_file.c

---
+++ practise/13.11.4-show_file.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NAMELEN 81
+
+int main(int argc, char * argv[])
+{
+    int file_num = 1;
+    int ch;
+
+    if (argc == 1)
+    {
+        fprintf(stderr, "Usage: %s [file names]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "Here is the first file entered in the command line:\n");
+
+    while (file_num++ < argc)
+    {
+        FILE * fs;
+        if ((fs = fopen(argv[file_num], "r")) == NULL)
+        {
+            fprintf(stderr, "Wrong in opening file %s.\n", argv[file_num]);
+            exit(EXIT_FAILURE);
+        }
+        while ((ch = getc(fs)) != EOF)
+            putc(ch, stdout);
+        fclose(fs);
+        fprintf(stdout, "Here is the next file:\n");
+    }
+    fprintf(stdout, "Done.\n");
+
+    system("pause");
+    return 0;
+}
