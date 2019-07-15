f0773d2fa702550747e3ed40a8fba4d9c39f1c7f
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-15 23:09:05 +0800

    a

--- practise/13.11.5-append_command_line.c
+++ practise/13.11.5-append_command_line.c
@@ -50,8 +50,10 @@ int main(int argc, char * argv[])
         {
             fputs("Can't create input buffer.\n", stderr);
         }
-        append(fs, fa);
         fprintf(fa, "\n\n");
+        
+        append(fs, fa);
+        
         if (ferror(fs) != 0)
             fprintf(stderr, "Error in reading file %s.\n", argv[file_num]);
         if (ferror(fa) != 0)
