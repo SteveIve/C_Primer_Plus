430204fbfe0971cfb3a1fa95b0364f0aac950069
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-17 21:50:55 +0800

    Create 13.11.7-two_files.c

---
+++ practise/13.11.7-two_files.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NAMELEN 81
+
+int main(int argc, char * argv[])
+{
+    FILE * f1, f2;
+    char name_1[NAMELEN];
+    char name_2[NAMELEN];
+    int ch;
+
+    
+}
