acac7495ff20f3a01d7dab109bd2ae07ba4db455
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-18 21:35:25 +0800

    Update 13.11.7.a-two_files.c

--- practise/13.11.7.a-two_files.c
+++ practise/13.11.7.a-two_files.c
@@ -4,5 +4,48 @@
 
 int main(int argc, char * argv[])
 {
+    FILE * f1, * f2;
+    int ch1, ch2;
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s [file1] [file2].\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((f1 = fopen(argv[1], "r")) == NULL)
+    {
+        fprintf(stderr, "Wrong in opening the file %s.\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
     
+    if ((f2 = fopen(argv[2], "r")) == NULL)
+    {
+        fprintf(stderr, "Wrong in opening the file %s.\n", argv[2]);
+        exit(EXIT_FAILURE);
+    }
+
+    ch1 = getc(f1);
+    ch2 = getc(f2);
+
+    while (ch1 != EOF || ch2 != EOF)
+    {
+        while (ch1 != EOF && ch1 != '\n')
+            putc(ch1, stdout);
+        if (ch1 != EOF)
+            putchar('\n');
+        while (ch2 != EOF && ch2 != '\n')
+            putc(ch2, stdout);
+        if (ch2 != EOF)
+            putchar('\n');
+    }
+
+    if (fclose(f1) != 0 || fclose(f2) != 0)
+    {
+        fprintf(stderr, "Wrong in closing file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    system("pause");
+    return 0;
 }
