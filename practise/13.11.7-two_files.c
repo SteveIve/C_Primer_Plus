a0bcfd25dbbdc09ab35e36aeabe414f714eb7edb
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-18 18:20:35 +0800

    Update 13.11.7-two_files.c

--- practise/13.11.7-two_files.c
+++ practise/13.11.7-two_files.c
@@ -9,5 +9,17 @@ int main(int argc, char * argv[])
     char name_2[NAMELEN];
     int ch;
 
-    if (argc == 2)
+    if (argc == 3)
+    {
+        if ((f1 = fopen(argv[1], "r")) == NULL)
+        {
+            fprintf(stderr, "Wrong in opening the first file.\n");
+            exit(EXIT_FAILURE);
+        }
+        if ((f2 = fopen(argv[2], "r")) == NULL)
+        {
+            fprintf(stderr, "Wrong in opening the second file.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
 }
