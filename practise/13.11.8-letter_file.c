d36836f844b94867b717023a5104b689a8ac0029
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-19 07:58:18 +0800

    Update 13.11.8-letter_file.c

--- practise/13.11.8-letter_file.c
+++ practise/13.11.8-letter_file.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define NAMELEN 81
 
 int main(int argc, char * argv[])
 {
     FILE * fp;
     int ch;
+
+    if (argc < 3)
+    {
+        if (argc == 1)
+        {
+            fprintf(stdout, "Please enter the checking letter:\n");
+            fscanf(stdin, "%c", &ch);
+            while (getchar() != '\n')
+                continue;
+            fprintf(stdout, "Now enter the file name;")
+        }
+    }
 }
