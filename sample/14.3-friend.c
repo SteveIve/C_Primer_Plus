7e3fabc4e5dd7f160d6a292cc9b7a1354843352e
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-19 23:43:22 +0800

    last one

--- sample/14.3-friend.c
+++ sample/14.3-friend.c
@@ -15,3 +15,36 @@ struct names {
     char last[LEN];
 };
 
+struct guy {
+    struct names handle;
+    char favfood[LEN];
+    char job[LEN];
+    float income;
+};
+
+int main(void)
+{
+    struct guy fellow = {
+        { "Ewen", "Villard" },
+        "grilled salmon", 
+        "personality coach", 
+        68112.00
+    };
+
+    printf("Dear %s, \n\n", fellow.handle.first);
+    printf("%s%s.\n", msgs[0], fellow.handle.first);
+    printf("%s%s\n", msgs[1], fellow.job);
+    printf("%s\n", msgs[2]);
+    printf("%s%s%s", msgs[3], fellow.favfood, msgs[4]);
+    if (fellow.income > 150000.0)
+        puts("!!");
+    else if (fellow.income > 75000.0)
+        puts("!");
+    else 
+        puts(".");
+    printf("\n%40s%s\n", " ", "See you soon,");
+    printf("%40s%s\n", " ", "Shalala");
+
+    system("pause");
+    return 0;
+}
