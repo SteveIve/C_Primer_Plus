e7c4f3ec4cfa16a95b2c6f5997211c32684451b2
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-19 23:28:08 +0800

    Update 14.2-manybook.c

--- sample/14.2-manybook.c
+++ sample/14.2-manybook.c
@@ -27,7 +27,7 @@ int main(void)
         s_gets(library[count].author, MAXAUTL);
         printf("Now enter the value.\n");
         scanf("%f", &library[count++].value);
-        while (getchar != '\n')
+        while (getchar() != '\n')
             continue;
         if (count < MAXBKS)
             printf("Enter the next title.\n");
@@ -47,4 +47,22 @@ int main(void)
 
     system("pause");
     return 0;
+}
+
+char * s_gets(char *st, int n)
+{
+    char * ret_val;
+    char * find;
+
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        find = strchr(st, '\n');
+        if (find)
+            *find = '\0';
+        else 
+            while (getchar() != '\n')
+                continue;
+    }
+    return ret_val;
 }
