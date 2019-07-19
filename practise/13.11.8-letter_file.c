88ab1a5a44062af6c088381196016518373df6a2
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-19 08:55:26 +0800

    Update 13.11.8-letter_file.c

--- practise/13.11.8-letter_file.c
+++ practise/13.11.8-letter_file.c
@@ -21,7 +21,54 @@ int main(int argc, char * argv[])
             fscanf(stdin, "%s", file_name);
             while (getchar() != '\n')
                 continue;
-            
+            if ((fp = fopen(file_name, "r")) == NULL)
+            {
+                fprintf(stderr, "Wrong in openging file.\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        if (argc == 2)
+        {
+            char file_name[NAMELEN];
+            if (strlen(argv[1]) != 1)
+            {
+                fprintf(stderr, "Please enter the correct parameter letter:\n");
+                fscanf(stdin, "%c", &ch);
+                while (getchar() != '\n')
+                    continue;
+            }
+            fprintf(stdout, "Please enter the file name:\n");
+            fscanf(stdin, "%s", file_name);
+            while (getchar() != '\n')
+                continue;
+            if ((fp = fopen(file_name, "r")) == NULL)
+            {
+                fprintf(stderr, "Wrong in opening file.\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    if (argc == 3)
+    {
+        ch = argv[1][0];
+        if (strlen(argv[1]) != 1)
+        {
+            fprintf(stderr, "Please enter the correct parameter letter:\n");
+            fscanf(stdin, "%c", &ch);
+            while (getchar() != '\n')
+                continue;
+        }
+        if ((fp = fopen(argv[2], "r")) == NULL)
+        {
+            fprintf(stderr, "Wrong in opening file.\n");
+            exit(EXIT_FAILURE);
+        }
+        int count;
+        int test;
+        for (count = 0, test = getc(fp); test != EOF; )
+        {
+            if (test == ch)
+                count++;
         }
     }
 }
