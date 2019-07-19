c6f10ade1ccac5fd23899f2c34cd26f3b1b5c375
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-19 23:33:12 +0800

    Create 14.3-friend.c

---
+++ sample/14.3-friend.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LEN 20
+const char * msgs[5] = 
+{
+    "    Thank you for the wonderful evening, ", 
+    "You certainly prove that a ", 
+    "is a special kind of guy. We must get together", 
+    "over a delicious ",
+    " and have a few laughs"
+};
+
+struct names {
+    char first[LEN];
+    char last[LEN];
+};
+
