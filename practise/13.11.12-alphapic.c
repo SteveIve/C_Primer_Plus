bef1cf911859001836fa85dfb2c89b5b65f1595e
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-19 11:42:00 +0800

    Update 13.11.12-alphapic.c

--- practise/13.11.12-alphapic.c
+++ practise/13.11.12-alphapic.c
@@ -8,61 +8,86 @@
 
 const char trans[LEVELS + 1] = " .':~*=&@#";
 
-void MakePic(int data[][COLS], char pic[][COLS], int rows);
-void init(char arr[][COLS], char ch);
-void makefile(char pic[][COLS], int rows, char *filename);
+void MakePic(int rows, int cols, int data[rows][cols], char pic[rows][cols]);
+void init(int rows, int cols, char arr[rows][cols], char ch);
+void makefile(int rows, int cols, char pic[rows][cols], char* filename);
 
 int main(void)
 {
-    int rows, cols;
-    int data[ROWS][COLS];
-    char pic[ROWS][COLS];
-    char filename[NAMELEN];
-    char picname[NAMELEN];
-    FILE * fp;
+	int rows, cols;
+	int data[ROWS][COLS];
+	char pic[ROWS][COLS];
+	char filename[NAMELEN];
+	char picname[NAMELEN];
+	FILE* fp;
 
-    init(pic, 'S');
+	init(ROWS, COLS, pic, 'S');
 
-    fprintf(stdout, "Enter the file name:\n");
-    fscanf(stdin, "%s", filename);
-    while (getchar() != '\n')
-        continue;
-    if ((fp = fopen(filename, "r")) == NULL)
-    {
-        fprintf(stderr, "Wrong in opening file.\n");
-        exit(EXIT_FAILURE);
-    }
-    strncpy(picname, filename, NAMELEN -5);
-    picname[NAMELEN - 6] = '\0';
-    strcat(picname, "_tran");
-    //此处读入数据
-    for (rows = 0; rows < ROWS; rows++)
-        for (cols = 0; cols < COLS; cols++)
-            fscanf(fp, "%d", &pic[rows][cols]);
-    if (ferror(fp))             //检查读入错误
-    {
-        fprintf(stderr, "Wrong in reading in the data.\n");
-        exit(EXIT_FAILURE);
-    }
-    MakePic(data, pic, 20);
+	fprintf(stdout, "Enter the file name:\n");
+	fscanf(stdin, "%s", filename);
+	while (getchar() != '\n')
+		continue;
+	if ((fp = fopen(filename, "r")) == NULL)
+	{
+		fprintf(stderr, "Wrong in opening file.\n");
+		exit(EXIT_FAILURE);
+	}
+	strncpy(picname, filename, NAMELEN - 5);
+	picname[NAMELEN - 6] = '\0';
+	strcat(picname, "_tran");
+	//此处读入数据
+	for (rows = 0; rows < ROWS; rows++)
+		for (cols = 0; cols < COLS; cols++)
+			fscanf(fp, "%d", &data[rows][cols]);
+	if (ferror(fp))             //检查读入错误
+	{
+		fprintf(stderr, "Wrong in reading in the data.\n");
+		exit(EXIT_FAILURE);
+	}
+	MakePic(ROWS, COLS, data, pic);
 
-    for (rows = 0; rows < ROWS; rows++)
-    {
-        for (cols  = 0; cols < COLS; cols++)
-            putchar(pic[rows][cols]);
-        putchar('\n');
-    }
+	for (rows = 0; rows < ROWS; rows++)
+	{
+		for (cols = 0; cols < COLS; cols++)
+			putchar(pic[rows][cols]);
+		putchar('\n');
+	}
 
-    makefile(pic, ROWS, picname);
+	makefile(ROWS, COLS, pic, picname);
 
-    system("pause");
-    return 0;
+	system("pause");
+	return 0;
 }
 
-void init(char arr[rows][cols], char ch)
+void init(int rows, int cols, char arr[rows][cols], char ch)
 {
     int r, c;
     for (r = 0; r < rows; r++)
         for (c = 0; c < cols; c++)
             arr[r][c] = ch;
+}
+void MakePic(int rows, int cols, int data[rows][cols], char pic[rows][cols])
+{
+	int r, c;
+	for (r = 0; r < rows; r++)
+		for (c = 0; c < cols; c++)
+			pic[r][c] = trans[data[r][c]];
+}
+
+void makefile(int rows, int cols, char pic[rows][cols], char* filename)
+{
+	FILE* fp;
+	int r, c;
+
+	if ((fp = fopen(filename, "w")) == NULL)
+	{
+		fprintf(stderr, "Wrong in creating pic file.\n");
+		exit(EXIT_FAILURE);
+	}
+	for (r = 0; r < rows; r++)
+	{
+		for (c = 0; c < cols; c++)
+			putc(pic[r][c], fp);
+		putc('\n', fp);
+	}
 }
