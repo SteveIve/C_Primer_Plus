4d6888ffc2fbf02a4f93f6e83f9aa913e2fd7480
SteveIve <42425136+SteveIve@users.noreply.github.com>
2019-07-26 19:43:05 +0800

    Update 14.10-names3.c

--- sample/14.10-names3.c
+++ sample/14.10-names3.c
@@ -10,4 +10,68 @@ struct namect {
 
 void getinfo(struct namect *);      //分配内存
 void makeinfo(struct namect *);
-void showinfo(struct namect *);
+void showinfo(const struct namect *);
+void cleanup(struct namect *);      //调用该函数时释放内存
+char * s_gets(char * st, int n);
+
+int main(void)
+{
+    struct namect person;
+
+    getinfo(&person);
+    makeinfo(&person);
+    showinfo(&person);
+    cleanup(&person);
+
+    system("pause");
+    return 0;
+}
+
+void getinfo(struct namect * pst)
+{
+    char temp[SLEN];
+    printf("Please enter your first name.\n");
+    s_gets(temp, SLEN);
+    pst->fname = (char *) malloc(strlen(temp) + 1);     //分配内存储存名
+    strcpy(pst->fname,temp);        //把名拷贝到动态分配的内存中
+    printf("Please enter your last name.\n");
+    s_gets(temp, SLEN);
+    pst->lname = (char *) malloc(strlen(temp) + 1);
+    strcpy(pst->lname, temp);
+}
+
+void makeinfo(struct namect * pst)
+{
+    pst->letters = strlen(pst->fname) + strlen(pst->lname);
+}
+
+void showinfo(const struct namect * pst)
+{
+    printf("%s %s, you name contains %d letters.\n",
+            pst->fname, pst->lname, pst->letters);
+}
+
+void cleanup(struct namect * pst)
+{
+    free(pst->fname);
+    free(pst->lname);
+}
+
+char * s_gets(char * st, int n)
+{
+    char * ret_val;
+    char * find;
+
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        find = strchr(st, '\n');
+        if (find)
+            *find = '\0';
+        else 
+            while (getchar() != '\n')
+                continue;
+    }
+
+    return ret_val;
+}
