b85c907bbf66e94aafabb9a18200d8a8973c99db
SteveIve <42425136+SteveIve@users.noreply.github.com>
2021-01-09 17:15:00 +0800

    update

--- Data_Structure/BinarySearch.c
+++ Data_Structure/BinarySearch.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#define maxsize 9999999
+#include <stdlib.h>
+#define maxsize 99999
 
 int binarySearch(int value, int *arr, int length)
 {
@@ -36,83 +37,88 @@ int main(void)
 {
     int binarySearch(int, int*, int);
 
-    int arr[13] = {5, 16, 39, 45, 51, 98, 100, 202, 226, 321, 368, 444, 501};
-    int flag = binarySearch(43, arr, 13);
-    int a = 43, b = 45, c = 202, d = 502;
-    if (flag >= 0)
-    {
-        printf("%d为数列中第%d个元素。\n", 43, flag);
-    }
-    else
-    {
-        printf("%d不在数列中。\n", 43);
-    }
-
-    flag = binarySearch(b, arr, 13);
-    if (flag >= 0)
-    {
-        printf("%d为数列中第%d个元素。\n", b, flag+1);
-    }
-    else
-    {
-        printf("%d不在数列中。\n", b);
-    }
-
-    flag = binarySearch(c, arr, 13);
-    if (flag >= 0)
-    {
-        printf("%d为数列中第%d个元素。\n", c, flag+1);
-    }
-    else
-    {
-        printf("%d不在数列中。\n", c);
-    }
-
-    flag = binarySearch(d, arr, 13);
-    if (flag >= 0)
-    {
-        printf("%d为数列中第%d个元素。\n", d, flag+1);
-    }
-    else
-    {
-        printf("%d不在数列中。\n", d);
-    }
-
-    // int arr[maxsize];
-    // for (int i = 0; i < maxsize; i++)
-    // {
-    //     arr[i-1] = i;
-    // }
-
-    // int flag = binarySearch(8, arr, maxsize);
+    // int arr[13] = {5, 16, 39, 45, 51, 98, 100, 202, 226, 321, 368, 444, 501};
+    // int flag = binarySearch(43, arr, 13);
+    // int a = 43, b = 45, c = 202, d = 502;
     // if (flag >= 0)
     // {
-    //     printf("%d为数列中第%d个元素。\n", 8, flag+1);
+    //     printf("%d为数列中第%d个元素。\n", 43, flag);
     // }
     // else
     // {
-    //     printf("%d不在数列中。\n", 8);
+    //     printf("%d不在数列中。\n", 43);
     // }
 
-    // flag = binarySearch(88, arr, maxsize);
+    // flag = binarySearch(b, arr, 13);
     // if (flag >= 0)
     // {
-    //     printf("%d为数列中第%d个元素。\n", 88, flag+1);
+    //     printf("%d为数列中第%d个元素。\n", b, flag+1);
     // }
     // else
     // {
-    //     printf("%d不在数列中。\n", 88);
+    //     printf("%d不在数列中。\n", b);
     // }
 
-    // flag = binarySearch(maxsize+1, arr, maxsize);
+    // flag = binarySearch(c, arr, 13);
     // if (flag >= 0)
     // {
-    //     printf("%d为数列中第%d个元素。\n", maxsize+1, flag+1);
+    //     printf("%d为数列中第%d个元素。\n", c, flag+1);
     // }
     // else
     // {
-    //     printf("%d不在数列中。\n", maxsize+1);
+    //     printf("%d不在数列中。\n", c);
     // }
+
+    // flag = binarySearch(d, arr, 13);
+    // if (flag >= 0)
+    // {
+    //     printf("%d为数列中第%d个元素。\n", d, flag+1);
+    // }
+    // else
+    // {
+    //     printf("%d不在数列中。\n", d);
+    // }
+
+
+
+
+    int arr[maxsize];
+    int i = 0;
+    while (i < maxsize)
+    {
+        arr[i] = i+1;
+        i++;
+    }
+
+    int flag = binarySearch(8, arr, maxsize);
+    if (flag >= 0)
+    {
+        printf("%d为数列中第%d个元素。\n", 8, flag+1);
+    }
+    else
+    {
+        printf("%d不在数列中。\n", 8);
+    }
+
+    flag = binarySearch(88, arr, maxsize);
+    if (flag >= 0)
+    {
+        printf("%d为数列中第%d个元素。\n", 88, flag+1);
+    }
+    else
+    {
+        printf("%d不在数列中。\n", 88);
+    }
+
+    flag = binarySearch(maxsize+1, arr, maxsize);
+    if (flag >= 0)
+    {
+        printf("%d为数列中第%d个元素。\n", maxsize+1, flag+1);
+    }
+    else
+    {
+        printf("%d不在数列中。\n", maxsize+1);
+    }
     
 
     system("pause");
