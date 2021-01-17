720541cdccd17b62a0d3d74e7e03da808c7575f4
SteveIve <42425136+SteveIve@users.noreply.github.com>
2021-01-17 14:19:04 +0800

    update

--- 2020/test.c
+++ 2020/test.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define maxsize 9999
 
-typedef struct test1
+typedef struct S1
 {
-    int value;
-    int index;
-}struct1;
+    double arr[maxsize];
+    struct S1 *next;    
+}MK;
 
-typedef struct test2
+int insert(MK *mk)
 {
-    int value;
-    int index;
-}struct2[10];
+    MK *node = malloc(sizeof(MK));
+    if (!node)
+    {
+        return 0;
+    }
+    else
+    {
+        node->next = mk;
+        mk = node;
+        return 1;
+    }
+    
+}
 
 int main(void)
 {
-    printf("struct1[10] occupies %d bits.\n", sizeof(struct1[10]));
-    printf("struct2 occupies %d bits.\n", sizeof(struct2));
+    MK *memorry_killer = malloc(sizeof(MK));
+    int i = 1;
+    while (i)
+    {
+        i = insert(memorry_killer);
+    }
 
+    printf("ÄÚ´æºÄ¾¡¡£\n");
     system("pause");
     return 0;
 }
